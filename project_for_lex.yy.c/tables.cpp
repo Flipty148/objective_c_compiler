9945292cfb9d68af953d7c7be9f643c6e4d1f09a
Yaroslav Marenkov <101563117+Flipty148@users.noreply.github.com>
2023-11-30 12:03:44 +0300

    Исправлены отсутствующее создание констант класса при добавлении класса

---
+++ project_for_lex.yy.c/tables.cpp
@@ -0,0 +1,237 @@
+#include "tables.h"
+#include <iostream>
+using namespace std;
+
+// -------------------- ConstantsTableElement --------------------
+
+ConstantsTableElement::ConstantsTableElement(int id, constantType type, string utf8string)
+{
+	Id = id;
+	Type = type;
+	Utf8String = utf8string;
+}
+
+
+ConstantsTableElement::ConstantsTableElement(int id, constantType type, int number, int firstRef, int secondRef)
+{
+	Id = id;
+	Type = type;
+	Number = number;
+	FirstRef = firstRef;
+	SecondRef = secondRef;
+}
+
+// -------------------- ConstantsTable --------------------
+
+ConstantsTable::ConstantsTable()
+{
+	items[maxId] = new ConstantsTableElement(maxId, UTF8, "Code");
+	maxId++;
+}
+
+int ConstantsTable::findOrAddConstant(constantType type, string utf8string)
+{
+	int res = findConstant(type, utf8string);
+	if (res == -1)
+	{
+		res = maxId++;
+		items[res] = new ConstantsTableElement(res, type, utf8string);
+	}
+	return res;
+}
+
+int ConstantsTable::findOrAddConstant(constantType type, int number, int firstRef, int secondRef)
+{
+	int res = findConstant(type, NULL, number, firstRef, secondRef);
+	if (res == -1)
+	{
+		res = maxId++;
+		items[res] = new ConstantsTableElement(res, type, number, firstRef, secondRef);
+	}
+	return res;
+}
+
+int ConstantsTable::findConstant(constantType type, string utf8string, int number, int firstRef, int secondRef)
+{
+	auto iter = items.cbegin();
+	while (iter != items.cend())
+	{
+		if (iter->second->Type == type && iter->second->Utf8String == utf8string && iter->second->Number == number && iter->second->FirstRef == firstRef && iter->second->SecondRef == secondRef)
+		{
+			return iter->first;
+		}
+		++iter;
+	}
+	return -1;
+}
+
+// -------------------- ClassesTableElement --------------------
+
+ClassesTableElement::ClassesTableElement(string name, string superclassName, bool isImplementation)
+{
+	ConstantTable = new ConstantsTable();
+	Fields = new FieldsTable();
+	Methods = new MethodsTable();
+	Properties = new PropertiesTable();
+	Name = ConstantTable->findOrAddConstant(UTF8, name);
+	SuperclassName = ConstantTable->findOrAddConstant(UTF8, superclassName);
+	ThisClass = ConstantTable->findOrAddConstant(Class, Name);
+	Superclass = ConstantTable->findOrAddConstant(Class, SuperclassName);
+	IsImplementation = isImplementation
+}
+
+// -------------------- ClassesTable --------------------
+void ClassesTable::addClass(string name, string superclassName, bool isImplementation)
+{
+	ClassesTableElement *element = new ClassesTableElement(name, superclassName, isImplementation);
+	items[name] = element;
+}
+
+// ------------------- FieldsTableElement --------------------
+
+FieldsTableElement::FieldsTableElement(int name, int descriptor, bool isInstance, Type *type)
+{
+	Name = name;
+	Descriptor = descriptor;
+	IsInstance = isInstance;
+	this->type = type;
+}
+
+// -------------------- FieldsTable --------------------
+
+void FieldsTable::addField(ConstantsTable *constantTable, string name, string descriptor, bool isInstance, Type *type)
+{
+	int NameId = constantTable->findOrAddConstant(UTF8, name);
+	int DescriptorId = constantTable->findOrAddConstant(UTF8, descriptor);
+	FieldsTableElement *field = new FieldsTableElement(NameId, DescriptorId, isInstance, type);
+	items[name] = field;
+}
+
+// -------------------- MethodsTableElement --------------------
+
+MethodsTableElement::MethodsTableElement(int name, int descriptor, bool isClassMethod, Statement_node* bodyStart, Type *returnType, vector<Type>* paramsTypes, vector<Type>* keywordsTypes)
+{
+	Name = name;
+	Descriptor = descriptor;
+	IsClassMethod = isClassMethod;
+	BodyStart = bodyStart;
+	ReturnType = returnType;
+	ParamsTypes = paramsTypes;
+	KeywordsTypes = keywordsTypes;
+	LocalVariables = new LocalVariablesTable();
+}
+
+// -------------------- MethodsTable --------------------
+
+void MethodsTable::addMethod(ConstantsTable *constantTable, string name, string descriptor, bool isClassMethod, Statement_node* bodyStart, Type *returnType, vector<Type>* paramsTypes, vector<Type>* keywordsTypes)
+{
+	if (items.count(name) != 0)
+	{
+		string msg = "Method '" + name + "' already exists";
+		throw new exception(msg.c_str());
+	}
+	int NameId = constantTable->findOrAddConstant(UTF8, name);
+	int DescriptorId = constantTable->findOrAddConstant(UTF8, descriptor);
+	MethodsTableElement *method = new MethodsTableElement(NameId, DescriptorId, isClassMethod, bodyStart, returnType, paramsTypes, keywordsTypes);
+	items[name] = method;
+}
+
+// -------------------- PropertiesTableElement --------------------
+
+PropertiesTableElement::PropertiesTableElement(int name, int descriptor, bool isReadonly, Type *type)
+{
+	Name = name;
+	Descriptor = descriptor;
+	IsReadonly = isReadonly;
+	this->type = type;
+}
+
+// -------------------- PropertiesTable --------------------
+
+ void PropertiesTable::addProperty(ConstantsTable *constantTable, string name, string descriptor, bool isReadonly, Type *type)
+ {
+	 if (items.count(name) != 0)
+	 {
+		 string msg = "Property '" + name + "' already exists";
+		 throw new exception(msg.c_str());
+	 }
+	 int NameId = constantTable->findOrAddConstant(UTF8, name);
+	 int DescriptorId = constantTable->findOrAddConstant(UTF8, descriptor);
+	 PropertiesTableElement *property = new PropertiesTableElement(NameId, DescriptorId, isReadonly, type);
+	 items[name] = property;
+ }
+
+ // ------------------- LocalVariablesTableElement --------------------
+
+ LocalVariablesTableElement::LocalVariablesTableElement(int id, string name, Type *type)
+ {
+	 Id = id;
+	 Name = name;
+	 this->type = type;
+ }
+
+// -------------------- LocalVariablesTable --------------------
+
+int LocalVariablesTable::findOrAddLocalVariable(string name, Type *type)
+{
+	if (items.count(name) == 0)
+	{
+		items[name] = new LocalVariablesTableElement(maxId++, name, type);
+	}
+	else {
+		string msg = "Variable '" + name + "' already exists";
+		throw new exception(msg.c_str());
+	}
+	return items[name]->Id;
+}
+
+// -------------------- Type --------------------
+
+Type::Type(type_type dataType, string className, int arrSize)
+{
+	DataType = dataType;
+	ClassName = className;
+	ArrSize = arrSize;
+}
+
+Type::Type(type_type dataType, string className)
+{
+	DataType = dataType;
+	ClassName = className;
+}
+
+Type::Type(type_type dataType)
+{
+	DataType = dataType;
+}
+
+Type::Type(type_type dataType, int arrSize)
+{
+	DataType = dataType;
+	ArrSize = arrSize;
+}
+
+// -------------------- FunctionsTableElement --------------------
+
+FunctionsTableElement::FunctionsTableElement(int name, int descriptor, Statement_node* bodyRoot)
+{
+	Name = name;
+	Descriptor = descriptor;
+	BodyRoot = bodyRoot;
+	LocalVariables = new LocalVariablesTable();
+}
+
+// -------------------- FunctionsTable --------------------
+
+void FunctionsTable::addFunction(ConstantsTable *constantTable, string name, string descriptor, Statement_node* bodyRoot)
+{
+	if (items.count(name) != 0)
+	{
+		string msg = "Function '" + name + "' already exists";
+		throw new exception(msg.c_str());
+	}
+	int NameId = constantTable->findOrAddConstant(UTF8, name);
+	int DescriptorId = constantTable->findOrAddConstant(UTF8, descriptor);
+	FunctionsTableElement *function = new FunctionsTableElement(NameId, DescriptorId, bodyRoot);
+	items[name] = function;
+}
---
+++ project_for_lex.yy.c/tables.h
@@ -0,0 +1,200 @@
+#include <iostream>
+#include <map>
+#include "classes_nodes.h"
+using namespace std;
+
+class LocalVariablesTable;
+class FieldsTable;
+class MethodsTable;
+class PropertiesTable;
+class Type;
+
+
+// ---------- Таблица констант ----------
+
+enum constantType {
+    UTF8,
+    Integer,
+    String,
+    Class,
+    NameAndType,
+    FieldRef,
+    MethodRef
+};
+
+class ConstantsTableElement
+{
+public:
+    int Id = NULL; // Номер константы
+    constantType Type; // Тип константы
+    string Utf8String = NULL; // Строка для значения UTF-8 констант
+    int Number = NULL; // Число для значения Integer констант
+    int FirstRef = NULL; // Ссылка на 1-ую константу 
+	int SecondRef = NULL; // Ссылка на 2-ую константу
+
+    ConstantsTableElement(int id, constantType type, string utf8string);
+    ConstantsTableElement(int id, constantType type, int number = NULL, int firstRef = NULL, int secondRef = NULL);
+};
+
+class ConstantsTable
+{
+public:
+    int maxId = 1; // Наибольший номер константы
+    map<int, ConstantsTableElement*> items; // Таблица констант
+
+    int findOrAddConstant(constantType type, string utf8string);
+    int findOrAddConstant(constantType type, int number = NULL, int firstRef = NULL, int secondRef = NULL);
+
+    ConstantsTable();
+private:
+    int findConstant(constantType type, string utf8string=NULL, int number=NULL, int firstRef=NULL, int secondRef=NULL);
+};
+
+// ---------- Таблица функций ----------
+
+class FunctionsTableElement
+{
+public:
+    int Name = NULL; // Ссылка на константу с именем функции
+    int Descriptor = NULL; // Ссылка на константу с дескриптором функции
+    Statement_node* BodyRoot = NULL; // Ссылка на узел начала тела функции
+	LocalVariablesTable* LocalVariables = NULL; // Ссылка на соответствующую таблицу локальных переменных
+
+    FunctionsTableElement(int name, int descriptor, Statement_node* bodyRoot);
+};
+
+class FunctionsTable
+{
+public:
+    map<string, FunctionsTableElement*> items; // Таблица функций, в качестве ключа - Имя функции
+
+    void addFunction(ConstantsTable *constantTable, string name, string descriptor, Statement_node* bodyRoot);
+};
+
+// ---------- Таблица классов ----------
+
+class ClassesTableElement
+{
+public:
+	int Name; // Ссылка на константу с именем класса
+    int SuperclassName; // Ссылка на константу с именем родительского класса
+    bool IsImplementation; //Флаг, который показывает является ли класс реализацией (ВОЗМОЖНО, СТОИТ ДОБАВИТЬ ИМЕННО НАЛИЧИЕ РЕАЛИЗАЦИИ КЛАССА)
+	int ThisClass; // Ссылка на константу текущего класса
+	int Superclass; // Ссылка на константу родительского класса
+	FieldsTable* Fields; // Ссылка на соответствующую таблицу полей класса
+	MethodsTable* Methods; // Ссылка на соответстующую таблицу методов класса
+	PropertiesTable* Properties; // Ссылка на соответствующую таблицу свойств класса
+    ConstantsTable* ConstantTable; // Таблица констант
+
+    ClassesTableElement(string name, string superclassName, bool isImplementation);
+};
+
+class ClassesTable
+{
+public:
+    map<string, ClassesTableElement*> items; // Таблица классов, в качестве ключа - Имя класса
+
+    void addClass(string name, string superclassName, bool isImplementation);
+};
+
+// ---------- Таблица полей класса ----------
+
+class FieldsTableElement
+{
+public:
+    int Name = NULL; // Ссылка на константу с именем поля
+	int Descriptor = NULL; // Ссылка на константу с дескриптором поля
+    bool IsInstance = NULL; // Флаг, показывающий является ли поле частью экземпляра класса
+    Type *type;
+
+    FieldsTableElement(int name, int descriptor, bool isInstance, Type *type);
+};
+
+class FieldsTable
+{
+public:
+    map < string, FieldsTableElement*> items; //Таблица полей класса, в качестве ключа - Имя поля класса
+
+    void addField(ConstantsTable *constantTable, string name, string descriptor, bool isInstance, Type *type);
+};
+
+// ---------- Таблица методов ----------
+
+class MethodsTableElement
+{
+public:
+    int Name = NULL; // Ссылка на константу с именем метода
+	int Descriptor = NULL; // Ссылка на константу с дескриптором метода (НУЖНО ЛИ НА KEYWORD ИЛИ ИХ НУЖНО ПРЕОБРАЗОВЫВАТЬ К СТАНДАРТНОМУ ДЕСКРИПТОРУ ФУНКЦИИ?)
+    bool IsClassMethod = NULL; // Флаг, который показывает принадлежность метода к классу, а не объекту
+	Statement_node* BodyStart = NULL; // Ссылка на узел начала тела метода
+	LocalVariablesTable* LocalVariables = NULL; // Ссылка на соотвветсвующую таблицу локальных переменных
+    Type *ReturnType; //Тип возвращаемого значепаарния
+    vector<Type> *ParamsTypes = NULL; //Тип параметров
+    vector<Type> *KeywordsTypes = NULL; //Тип параметров keyword
+
+    MethodsTableElement(int name, int descriptor, bool isClassMethod, Statement_node* bodyStart, Type *returnType, vector<Type>* paramsTypes, vector<Type>* keywordsTypes);
+};
+
+class MethodsTable
+{
+public:
+	map< string, MethodsTableElement* > items; //Таблица методов класса, в качестве ключа - Имя метода класса
+
+    void addMethod(ConstantsTable *constantTable, string name, string descriptor, bool isClassMethod, Statement_node* bodyStart, Type *returnType, vector<Type>* paramsTypes, vector<Type>* keywordsTypes);
+};
+
+// ----------- Таблица свойств ----------
+
+class PropertiesTableElement
+{
+public:
+    int Name = NULL; // Ссылка на константу с именем свойства
+	int Descriptor = NULL; // Ссылка на константу с дескриптором типа константы
+    bool IsReadonly = NULL; // Флаг, который показывает, что свойство доступно только для чтения
+    Type *type; // Тип свойства
+
+    PropertiesTableElement(int name, int descriptor, bool isReadonly, Type *type);
+};
+
+class PropertiesTable
+{
+public:
+	map<string, PropertiesTableElement*> items; // Таблица  свойств класса, в качестве ключа - Имя свойства класса
+
+    void addProperty(ConstantsTable *constantTable, string name, string descriptor, bool isReadonly, Type *type);
+};
+
+// ---------- Таблица локальных переменных ----------
+
+class LocalVariablesTableElement
+{
+public:
+	int Id = NULL; // Номер локальной переменной
+	string Name = NULL; // Имя локальной переменной
+    Type *type; //Тип переменной
+
+    LocalVariablesTableElement(int id, string name, Type *type);
+};
+
+class LocalVariablesTable
+{
+public:
+    int maxId = 0; // Наибольший номер локальной переменной
+	map<string, LocalVariablesTableElement*> items; // Таблица локальных переменных, в качестве ключа - Имя локальной переменной
+
+    int findOrAddLocalVariable(string name, Type *type);
+};
+
+// ---------- Типы ----------
+class Type 
+{
+public:
+    type_type DataType; // Тип данных
+    string ClassName; //Имя класса
+    int ArrSize; // Размер массива
+
+    Type(type_type dataType, string className, int arrSize);
+    Type(type_type dataType, string className);
+    Type(type_type dataType);
+    Type(type_type dataType, int arrSize);
+};

b83a33b0b242bd2e69a03fe31546da273dd947dc
Flipty148 <101563117+Flipty148@users.noreply.github.com>
2023-12-03 12:27:23 +0300

    Добавлены методы RTL для печати

---
+++ semantics/RTLJava/default/Program.java
@@ -0,0 +1,23 @@
+public class Program {
+
+    //---------- Print Methods ----------
+    public static void print(int i) {
+        System.out.print(i);
+    }
+
+    public static void print(char c) {
+        System.out.print(c);
+    }
+
+    public static void print(String s) {
+        System.out.print(s);
+    }
+
+    public static void print(char[] s) {
+        System.out.print(s);
+    }
+
+    public static void print(Object o) {
+        System.out.print(o);
+    }
+}

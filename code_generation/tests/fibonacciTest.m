196cdfb996375644e624912445ed111579381e8f
Flipty148 <101563117+Flipty148@users.noreply.github.com>
2024-01-20 13:00:00 +0300

    Добавлен тест на рекурсивное нахождение числа Фибоначчи

---
+++ code_generation/tests/fibonacciTest.m
@@ -0,0 +1,21 @@
+@class InOutFuncs;
+@implementation Fibonacci : NSObject
+
++ (int) nElem: (int) number {
+    if (number == 1)
+        return 1;
+    else if (number == 2)
+        return 1;
+    else 
+        return [self nElem: number - 1] + [self nElem: number - 2];
+}
+
+@end
+
+int main ()
+{
+    [InOutFuncs printCharArray: "Input number:"];
+    int n = [InOutFuncs readInt];
+    [InOutFuncs printInt: [Fibonacci nElem: n]];
+    return 0;
+}

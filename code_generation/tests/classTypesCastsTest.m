a3eb04a7cde41b2f2df6a2567fb541fb44d2eff2
Yaroslav Marenkov <101563117+Flipty148@users.noreply.github.com>
2024-01-29 12:06:48 +0300

    Исправлен тестов

--- code_generation/tests/classTypesCastsTest.m
+++ code_generation/tests/classTypesCastsTest.m
@@ -2,12 +2,6 @@
 @end
 
 @implementation MyClass2 : MyClass1
-- (void) func {
-    return;
-}
-@end
-
-@implementation MyClass3 : NSObject
 @end
 
 int main ()
@@ -17,14 +11,4 @@ int main ()
     
     MyClass2 *obj3 = [MyClass2 new];
     MyClass2 *obj4 = obj3;
-    
-    //В оригинальном компиляторе такое возможно. Однако, JVM не разрешает использовать различные типы для создания и инициализации объекта.
-    //Поэтому у меня запрещено преобразовывать типы классов, если они не являются родительскими или дочерними.
-    // MyClass2 *obj5 = [MyClass3 new];
-    // MyClass2 *obj6 = obj5;
-    // MyClass2 *obj5;
-    // obj5 = [MyClass3 new];
-
-    // //Ошибки
-    // [obj5 func];
 }
--- code_generation/tests/fastEnumerationTest.m
+++ code_generation/tests/fastEnumerationTest.m
@@ -9,11 +9,6 @@ int main() {
     arr1 = [arr1 arrayByAddingObject: @"five"];
     arr1 = [arr1 arrayByAddingObject: @"six"];
 
-    NSArray *arr2 = [NSArray array];
-    arr2 = [arr2 arrayByAddingObject: @"seven"];
-    arr2 = [arr2 arrayByAddingObject: @"eight"];
-    arr2 = [arr2 arrayByAddingObject: @"nine"];
-
     // NSString *elem;
     for (NSString *elem in arr)
     {
@@ -21,8 +16,6 @@ int main() {
         for (NSString *e in arr1)
         {
             [InOutFuncs printNSString:e];
-            //arr1 = [arr1 arrayByAddingObject: @"test"];
-            arr1 = arr2;
         }
     }
 }
